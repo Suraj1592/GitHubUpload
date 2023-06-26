9a2ac0397c91c3ccef1fc914fcb946b07edc8a66
Suraj Pawar <precatsuraj2023@gmail.com>
2023-06-26 17:56:18 +0530

    msg

---
+++ src/demo/demo1.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class demo1 
+{
+	public static void main(String[]args)
+	{
+		System.out.println("This is the first prog to get upload in git");
+	}
+}
---
+++ src/demo/demo2.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class demo2 
+{
+	public static void main(String[]args)
+	{
+		System.out.println("This is the SECOND prog to get upload in git");
+		System.out.println("Hello Demo2");
+	}
+}
